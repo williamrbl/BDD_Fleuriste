715b1f6f388ed307a37047abd79d172957b48d25
william.rbl <will.rabolin@orange.fr>
2024-05-18 12:54:02 +0200

    Commit

--- Projet/obj/Release/net6.0/Projet.AssemblyInfo.cs
+++ Projet/obj/Release/net6.0/Projet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b7b84c0533692bca5fa9773f532eed6a8a2a738")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
